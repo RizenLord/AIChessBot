93e56b477652e7685590cce36f8ac6638308dd5d
Tyson Keith <rizenlordgaming@gmail.com>
2023-05-04 13:19:11 -0400

    Prompt Fix for the getGPTmove function

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ def getGPTmove(moves, legalMoves):
                 model="gpt-3.5-turbo",
                 temperature=0,
                 max_tokens=6,
-                prompt="Play Chess: " + ', '.join(moves),
+                prompt="Play Chess Against Me: " + ', '.join(moves),
             )
+            print(selectedMove)
             if chess.Move.from_uci(selectedMove) in legalMoves:
                 board.push_san(selectedMove)
                 moveCount += 1
